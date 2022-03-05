92b4ac13c217acb4e3ede9287ce9bf743e9ca209
Teri11 <augustineteri157@gmail.com>
2022-03-05 20:05:17 +0300

    added registration blueprint auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from config import config_options
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -10,11 +13,20 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    app.config['SECRET_KEY'] = 'secretkey'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
+      # configure UploadSet
+    configure_uploads(app,photos)
+    
     # Registering the blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
