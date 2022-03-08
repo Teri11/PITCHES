1ac3585097071f2dd4997fc8d595a483bc8fe5d7
Teri11 <augustineteri157@gmail.com>
2022-03-08 22:21:32 +0300

    changed manage.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy.model import Model
+# from flask_sqlalchemy.model import Model
 from . import db,login_manager
 from datetime import datetime
 from flask_login import UserMixin,current_user
